8fcd7255b9745479e9b3e62869406db15f651fb8
abhijeetchawla <abhijeetchawla22@gmail.com>
2021-09-02 18:24:12 +0530

    basic commands added

---
+++ Ex7TasksCLI/cmd/do.go
@@ -0,0 +1,42 @@
+package cmd
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/spf13/cobra"
+)
+
+// doCmd represents the do command
+var doCmd = &cobra.Command{
+	Use:   "do",
+	Short: "Mark a task on your TODO list as complete",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			fmt.Println("This command requires and argument to work")
+			return
+		}
+
+		doneTask, err := strconv.Atoi(args[0])
+		if err != nil {
+			fmt.Println("The argument passed is not a number")
+			return
+		}
+
+		fmt.Printf("This task will be done :%d \n", doneTask)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(doCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// doCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// doCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
